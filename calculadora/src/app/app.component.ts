60014d7761cc3246adb590d2b17a7c78f41a9c1f
juliane <fbjuliane6@gmail.com>
2026-04-23 10:45:04 -0300

    dando ação ao layout da calculadora

--- calculadora/src/app/app.component.ts
+++ calculadora/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MinhacalculadoraComponent } from './minhacalculadora/minhacalculadora.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MinhacalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- calculadora/src/app/minhacalculadora/minhacalculadora.component.ts
+++ calculadora/src/app/minhacalculadora/minhacalculadora.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-minhacalculadora',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule], 
   templateUrl: './minhacalculadora.component.html',
-  styleUrl: './minhacalculadora.component.scss'
+  styleUrls: ['./minhacalculadora.component.scss'] 
 })
 export class MinhacalculadoraComponent {
 
+  numero1: number = 0;
+  numero2: number = 0;
+
+  resultado: number = 0;
+
+  calcularResultado(){
+    this.resultado = this.numero1 + this.numero2;
+  }
 }
